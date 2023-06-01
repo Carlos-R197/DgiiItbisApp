9a1a0e11314d91c10da5001b50ec108b35a2b851
Carlos Roque <carlosroque198@gmail.com>
2023-06-01 14:50:12 -0400

    Created a new test for the TaxReceiptController

--- Backend/Api.UnitTests/TaxReceiptControllerTests.cs
+++ Backend/Api.UnitTests/TaxReceiptControllerTests.cs
@@ -12,26 +12,41 @@ namespace Api.UnitTests;
 
 public class TaxReceiptControllerTests
 {
+    private readonly Mock<ITaxReceiptRepository> taxRepoStub = new();
+    private readonly Mock<IContributorRepository> contributorRepoStub = new();
+    private readonly Mock<ILogger<TaxReceiptController>> loggerStub = new();
+
     [Fact]
     public async void GetTaxReceiptsAsync_WithExistingContributor_ReturnsOk()
     {
         // Arrange
-        var taxRepoStub = new Mock<ITaxReceiptRepository>();
-        taxRepoStub
-            .Setup(t => t.GetTaxReceiptsAsync())
+        taxRepoStub.Setup(t => t.GetTaxReceiptsAsync())
             .ReturnsAsync(new List<TaxReceipt>());
-        var contributorRepoStub = new Mock<IContributorRepository>();
-        contributorRepoStub
-            .Setup(t => t.ContributorExistsAsync("52987455224"))
+        contributorRepoStub.Setup(t => t.ContributorExistsAsync("52987455224"))
             .ReturnsAsync(true);
-        var loggerStub = new Mock<ILogger<TaxReceiptController>>();
         
         var controller = new TaxReceiptController(taxRepoStub.Object, contributorRepoStub.Object, loggerStub.Object);
         // Act
-        var result = await controller.GetTaxReceiptsAsync();
+        var result = await controller.GetTaxReceiptsAsync("52987455224");
         var okObjectResult = result.Result as OkObjectResult;
         // Assert 
         Assert.IsType<OkObjectResult>(result.Result);
         Assert.IsAssignableFrom<IEnumerable<TaxReceipt>>(okObjectResult.Value);
+    }
+
+    [Fact]
+    public async void GetTaxReceiptsAsync_WithInvalidContributor_ReturnsNotFound()
+    {
+        // Arrange
+        taxRepoStub.Setup(t => t.GetTaxReceiptsAsync())
+            .ReturnsAsync(new List<TaxReceipt>());
+        contributorRepoStub.Setup(t => t.ContributorExistsAsync("69921108970"))
+            .ReturnsAsync(false);
+        
+        var controller = new TaxReceiptController(taxRepoStub.Object, contributorRepoStub.Object, loggerStub.Object);
+        // Act
+        var result = await controller.GetTaxReceiptsAsync("69921108970");
+        // Assert 
+        Assert.IsType<NotFoundResult>(result.Result);
     }
 }
